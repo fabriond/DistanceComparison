f5af4bed948f17d0406d7efcd307b98308449386
fabriond <gfbf@ic.ufal.br>
2019-04-14 05:17:10 -0300

    Added shortest route calculation

--- lib/distance_comparison.ex
+++ lib/distance_comparison.ex
@@ -9,6 +9,7 @@ defmodule DistanceComparison do
 
   @url "https://maps.googleapis.com/maps/api/distancematrix/json?"
 
+  require Logger
   @doc """
   Sends the request to the google distance matrix API to return the distances
   from the origin(s) to the destination(s)
@@ -20,6 +21,7 @@ defmodule DistanceComparison do
 
     {:ok, resp} = HTTPoison.get(@url <> params(paths, options))
     Poison.decode!(resp.body)
+    |> check_distance()
   end
 
   defp params(%Paths{} = paths, options) do
@@ -27,5 +29,72 @@ defmodule DistanceComparison do
     |> Map.merge(Paths.to_params(paths))
     |> Map.merge(options)
     |> URI.encode_query()
+  end
+
+# Returns a map containing the best routes based on the map that returns from
+# the google distance matrix API
+  def check_distance(%{status: "OK", origin_addresses: origins, destination_addresses: destinations, rows: rows}) do
+    res = update_origin(origins, destinations, rows)
+
+    hd Enum.sort(res, fn(%{distance: d1}, %{distance: d2}) -> d1 < d2 end)
+  end
+
+  def check_distance(%{status: status}) do
+    raise RuntimeError, status <> " distance matrix response status, please " <>
+    "check https://developers.google.com/maps/documentation/distance-matrix/intro#StatusCodes"
+  end
+
+# Gets the best destination for all origins using the update_destination/3
+# function below
+  defp update_origin(origins, destinations, rows, result \\ [])
+
+  defp update_origin([], _destinations, [], result) do
+    result
+  end
+
+  defp update_origin(origins, destinations, rows, result) do
+    [current_row | rows] = rows
+    [current_origin | origins] = origins
+    %{elements: elements} = current_row
+
+    result = [update_destination(current_origin, destinations, elements) | result]
+    update_origin(origins, destinations, rows, result)
+  end
+
+# Gets the best destination for an origin based on the destinations list and the
+# distance in the elements list (which comes from the google distance matrix API)
+  defp update_destination(current_origin, destinations, elements, result \\ %{})
+
+  defp update_destination(_current_origin, [], [], result)do
+    result
+  end
+
+  defp update_destination(current_origin, destinations, elements, result) do
+    [current_element | elements] = elements
+    [current_destination | destinations] = destinations
+
+    current_distance =
+    if match?(%{status: "OK"}, current_element) do
+      current_element.distance.value
+    else
+      Logger.error("element status: " <> current_element.status <> ", path " <>
+                   "\"#{current_origin}\" -> \"#{current_destination}\" " <>
+                   "will be discarded from the calculations")
+      :invalid
+    end
+
+    result =
+    if Map.has_key?(result, :distance) do
+      old_distance = result.distance
+      if current_distance != :invalid and current_distance < old_distance do
+        %{origin: current_origin, destination: current_destination, distance: current_distance}
+      else
+        result
+      end
+    else
+      %{origin: current_origin, destination: current_destination, distance: current_distance}
+    end
+
+    update_destination(current_origin, destinations, elements, result)
   end
 end
--- test/distance_comparison_test.exs
+++ test/distance_comparison_test.exs
@@ -2,7 +2,22 @@ defmodule DistanceComparisonTest do
   use ExUnit.Case
   doctest DistanceComparison
 
-#  test "greets the world" do
-#    assert DistanceComparison.hello() == :world
-#  end
+  @input_map1 %{status: "OK", origin_addresses: ["Vancouver, BC, Canada"], destination_addresses: ["San Francisco, California, United States", "Victoria, BC, Canada"],
+                rows: [%{elements: [%{status: "OK", distance: %{value: 1734542, text: "1735km"}}, %{status: "OK", distance: %{value: 129324, text: "129km"}}]}]}
+
+  @expected_output1 %{destination: "Victoria, BC, Canada", distance: 129324, origin: "Vancouver, BC, Canada"}
+
+  @input_map2 %{status: "OK", origin_addresses: ["San Francisco, California, United States", "Victoria, BC, Canada"], destination_addresses: ["Vancouver, BC, Canada"],
+                rows: [%{elements: [%{status: "OK", distance: %{value: 1734542, text: "1735km"}}]}, %{elements: [%{status: "OK", distance: %{value: 129324, text: "129km"}}]}]}
+
+  @expected_output2 %{destination: "Vancouver, BC, Canada", distance: 129324, origin: "Victoria, BC, Canada"}
+
+  test "check_distance test for a single origin" do
+    assert DistanceComparison.check_distance(@input_map1) == @expected_output1
+  end
+
+  test "check_distance test for a single destination" do
+    assert DistanceComparison.check_distance(@input_map2) == @expected_output2
+  end
+
 end
